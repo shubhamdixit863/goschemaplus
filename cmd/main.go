bdac8a2979379c041963fc8f6c38d845eb882cb7
shubhamdixit <mobcoderid-170@192.168.1.4>
2021-12-19 13:45:00 +0530

    structural fixes

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/jono-tt/generate/pkg/converter"
+)
+
+func main() {
+
+	converter.Convert()
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jono-tt/generate
+
+go 1.17
